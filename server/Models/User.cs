459350ccfa1884a56a337312f951a5c3e4017d1c
Nguyễn Huỳnh Long <huynhlong141003@gmail.com>
2024-07-20 17:40:10 +0700

    refactor folder models

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -1,39 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Backend.Models;
-
-public partial class User
+namespace Backend.Models
 {
-    public int Id { get; set; }
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } = null!;
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; } = null!;
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; } = null!;
 
-    public string Name { get; set; } = null!;
+        [Required]
+        [StringLength(10)]
+        public string Phone { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
+        public DateTime? CreateAt { get; set; }
 
-    public string Username { get; set; } = null!;
+        public DateTime? ModifiedAt { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
 
-    public string Password { get; set; } = null!;
 
-    public string Phone { get; set; } = null!;
-
-    public DateTime? CreateAt { get; set; }
-
-    public DateTime? ModifiedAt { get; set; }
-
-    public bool IsActive { get; set; }
-
-    public int? RoleId { get; set; }
-
-    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
-
-    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
-
-    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
-
-    public virtual ICollection<PurchaseOrder> PurchaseOrders { get; set; } = new List<PurchaseOrder>();
-
-    public virtual Role? Role { get; set; }
-
-    public virtual ICollection<UsersAddress> UsersAddresses { get; set; } = new List<UsersAddress>();
+    }
 }
